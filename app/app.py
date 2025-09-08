6c4c45f9199ec98bcf12b21b743b71aaf4bfea0d
Yatesh Ingale <vickyingale2000@gmail.com>
2025-09-08 18:13:04 +0530

    Test webhook

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD Pipeline! 1st project done by yatesh"
+    return "Hello, CI/CD Pipeline!  project done by yatesh"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
