7c48bdcbfb8a678a0ad937b051f5dc0e9b402716
Yatesh Ingale <vickyingale2000@gmail.com>
2025-09-09 16:23:30 +0530

    testing

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Testing webhook again"
+    return "Testing webhook"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
