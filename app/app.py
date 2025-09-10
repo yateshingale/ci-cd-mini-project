f9ee0631ea89915f19001b47bc79558192ae1323
Yatesh Ingale <vickyingale2000@gmail.com>
2025-09-10 12:03:35 +0530

    testing

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "finally webhook working with ngrok"
+    return "finally webhook working with jenkins"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
