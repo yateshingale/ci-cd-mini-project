13f59c79812ec4b137839e87e2998d83f0a35583
Yatesh Ingale <vickyingale2000@gmail.com>
2025-09-10 11:48:07 +0530

    webhook test

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Testing webhook"
+    return "finally webhook working with ngrok"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
